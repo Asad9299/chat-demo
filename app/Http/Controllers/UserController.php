37d549635655c50c1903afa35bfddb2d358b9088
Asad0112 <asad.mansuri@matrixhive.com>
2022-12-16 16:34:24 +0530

    Chat Demo - Final Commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    public function list(Request $request)
+    {
+        $users = User::where('id', '!=', Auth::id())->get();
+        return $users;
+    }
+}
